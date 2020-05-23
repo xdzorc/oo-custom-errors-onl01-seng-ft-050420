21945fee2524b3359f34fb259f6c38092dfc900c
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 23:06:59 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,7 +10,7 @@ class Person
     if person.class != Person
       begin
         raise PartnerError
-      rescue PartnerError 
+      rescue PartnerError => error
           puts error.message
       end
     else
