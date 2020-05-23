756b6b92ddbe509ad686db3ec8feaaa0db2ebe3c
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 23:00:23 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,23 +1,23 @@
 class Person
   attr_accessor :partner, :name
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
-
+ 
   class PartnerError < StandardError
   end
 end
-
+ 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
